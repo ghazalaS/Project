8be16f8a5b284a55a7cbc2aa5132beb466150949
ghazalaS <ghazala.s.95@gmail.com>
2017-03-23 18:50:58 +0500

    2

--- Project/settings.py
+++ Project/settings.py
@@ -123,4 +123,10 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.9/howto/static-files/
 
 STATIC_URL = '/static/'
+
+PROJECT_ROOT = os.path.dirname(os.path.abspath(__file__))
+STATIC_ROOT = os.path.join(PROJECT_ROOT, 'staticfiles')
+STATICFILES_DIRS = (os.path.join(PROJECT_ROOT, 'static'),)
+STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
+
 AUTH_USER_MODEL = 'first.AUser'
